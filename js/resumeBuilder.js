51b78bb0d5790165b62faffcb10147f7886d0445
Javier Pascual <javier.x.pascual@nielsen.com>
2015-07-10 11:09:05 +0200

    work experience complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,26 +1,37 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
 var work = {
   "jobs": [
     {
       "employer": "Nielsen Neuro",
       "title" : "Software Engineer",
-      "dates": "2014-Present",
-      "description": ""
+      "dates": "March 2013 - Present",
+      "location" : "Berlin, Germany",
+      "description": "Design and implement features for an EEG recording software. Analyze EEG data."
     },
     {
       "employer": "TU Berlin",
       "title" : "Research Assistant",
-      "dates": "2011-2013",
-      "description": ""
+      "dates": "April 2010 - February 2013",
+      "location" : "Berlin, Germany",
+      "description": "Design and implement a Brain Computer Interface for object selection, integrated in a neuroprosthesis. Research and develope new methods to remove afferent activity caused by Neuromuscular Electrical Stimulation."
+    },
+    {
+      "employer": "NTR Global",
+      "title" : "Software Engineer",
+      "dates": "July 2006 - March 2009",
+      "location" : "Barcelona, Spain",
+      "description": "Design and develop of new functionalities for a Remote Control Software."
+    },
+    {
+      "employer": "Tango/04",
+      "title" : "Software Engineer",
+      "dates": "May 2002 - September 2005",
+      "location" : "Barcelona, Spain",
+      "description": "Developing and maintaining a network devices real-time monitoring framework."
     }
   ],
 };
 
 var projects = {
-
 };
 
 var education = {
@@ -34,21 +45,21 @@ var education = {
   ],
   "onlineCourses" : [
     {
-      "title": "",
-      "school": "",
-      "dates": ""
+      "title": "Programming Mobile Applications for Android Handheld Systems: Part 1",
+      "school": "University of Maryland (coursera.org)",
+      "dates": "June 2015"
     }
   ]
 };
 
- var bio = {
-   "name" : "Javier Pascual",
-   "role" : "Software Engineer",
-   "contacts" : {"email" : "javipascualg@gmail.com", "location":"Berlin"},
-   "pic_url" : "images/fry.jpg",
-   "welcome-message" : "Hey!",
-   "skills" : ["C++", "JS", "Matlab", "Python"]
- };
+var bio = {
+ "name" : "Javier Pascual",
+ "role" : "Software Engineer",
+ "contacts" : {"email" : "javipascualg@gmail.com", "location":"Berlin"},
+ "pic_url" : "images/fry.jpg",
+ "welcome-message" : "Hey!",
+ "skills" : ["C++", "JS", "Matlab", "Python"]
+};
 
 
 
@@ -63,10 +74,13 @@ var education = {
  }
 
  for (i=0;i<work.jobs.length;i++){
-   $("workExperience").append(HTMLworkStart);
+   $("#workExperience").append(HTMLworkStart);
    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
-   $("#workExperience:last").append(formattedEmployer+formattedTitle);
+   var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+   var formattedLocation = HTMLworkLocation .replace("%data%", work.jobs[i].location);
+   var formattedDescr = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+   $(".work-entry:last").append(formattedEmployer+formattedTitle+formattedDates+formattedLocation+formattedDescr);
  }
 
  var formattedName = HTMLheaderName.replace("%data%", bio.name);
